5dc231fef4f169eb3116caafd6a77106dbaaf801
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-02-07 15:22:56 +0000

    Added Bulma and styled the Hero Page with title and background

--- react-project-starterpack/src/app.js
+++ react-project-starterpack/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import 'bulma'
 import './styles/main.scss'
 
 import Home from './common/Home'
--- react-project-starterpack/src/common/Home.js
+++ react-project-starterpack/src/common/Home.js
@@ -1,20 +1,16 @@
 import React from 'react'
 
-class Home extends React.Component { 
+const Home = () => (
 
-  render() {
-    return (
-      <section className="hero is-fullheight-with-navbar is-primary">
-        <div className="hero-body">
-          <div className="container">
-            <p className="title is-one has-text-centred">GEO-CACHE</p>
-            <p className="has-text-centred">The largest treassure Hunt Community in London!</p>
-          </div>
-        </div>
-      </section>
-    )
-  }
-
-}
+  <section className="hero is-fullheight-with-navbar is-primary">
+    <div className="hero-body">
+      <div className="container">
+        <p className="title is-1 has-text-centered" id="Hero_txt">GEO-CACHE</p>
+        <p className=" title is-2 has-text-centered " id="Hero_txt">The largest treassure Hunt Community in London!</p>
+      </div>
+    </div>
+  </section>
+)
+  
 
 export default Home
