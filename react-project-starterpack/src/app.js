c3160ebe99e0e46b4245ebbd92de1d62042d0ae4
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-07 16:50:02 +0000

    login page started

---
+++ react-project-starterpack/src/app.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'bulma'
+import './styles/main.scss'
+import Register from './auth/Register'
+import Login from './auth/Login'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+
+
+
+const App = () => (
+  <BrowserRouter>
+    <main>
+      <Switch>
+        
+        <Route path="/register"component={Register}/>
+        <Route path="/login"component={Login} />
+        
+      </Switch>
+
+    </main>
+  </BrowserRouter>
+  
+)
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ react-project-starterpack/src/auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+
+
+const Login = () => (
+  <main>
+    <h1>hey</h1>
+  </main>
+  
+)
+
+export default Login
