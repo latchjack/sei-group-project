a827c02534c55a0f4494f561eb25fcbeea6cb7ff
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-10 17:12:35 +0000

    auth page

--- react-project-starterpack/src/components/common/Navbar.js
+++ react-project-starterpack/src/components/common/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends React.Component {
             <Link className="navbar-item" to="/">Home </Link>
             <Link className="navbar-item" to="/trails">Trail Index</Link>
             <Link className="navbar-item" to="/FAQ">FAQ</Link>
-            <Link className="navbar-item" to="/refister">Register</Link>
+            <Link className="navbar-item" to="/register">Register</Link>
             <Link className="navbar-item" to="/login">Login</Link>
             <Link className="navbar-item" to="/trails/new">Make a new trail</Link>
           </div>
