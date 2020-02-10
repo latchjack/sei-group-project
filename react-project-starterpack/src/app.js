c6f871969d3b9fd493b883d40c10eb722e852de4
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-10 11:11:17 +0000

    re-ordered homepage

--- react-project-starterpack/src/app.js
+++ react-project-starterpack/src/app.js
@@ -2,16 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bulma'
 import './styles/main.scss'
-import Register from './auth/Register'
-import Login from './auth/Login'
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import TrailNew from './trails/TrailNew'
-import CompleteForm from './trails/CompleteForm'
-
 import Home from './common/Home'
 import Navbar from './common/Navbar'
 import Trails from './trails/Trails'
 import TrailCard from './trails/TrailCard'
+import CompleteForm from './trails/CompleteForm'
+import Register from './auth/Register'
+import Login from './auth/Login'
 
 class App extends React.Component {
   render() {
@@ -20,13 +19,15 @@ class App extends React.Component {
         <Navbar/>
         <main>
           <Switch>
-            <Route path="/trails/:id" component={TrailCard}/>
+            <Route exact path="/"component={Home}/>
+            <Route path="/trails/new"component={TrailNew} />
             <Route path="/trails" component={Trails}/>
-            <Route exact path="/" component={Home}/>
+            <Route path="/trails/:id" component={TrailCard}/>
+            <Route path="/trails/:id/complete"component={CompleteForm}/>
             <Route path="/register"component={Register}/>
             <Route path="/login"component={Login} />
-            <Route path="/trails/new"component={TrailNew} />
-            <Route path="/trails/:id/complete"component={CompleteForm}/>
+            
+            
           </Switch>
         </main>
       </BrowserRouter>
