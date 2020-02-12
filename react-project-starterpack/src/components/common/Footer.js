d2a33b9dc09f90e9d6f293afd167c6ba220b8903
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-02-12 14:37:04 +0000

    Footer

--- react-project-starterpack/src/components/common/Footer.js
+++ react-project-starterpack/src/components/common/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+
 class Footer extends React.Component {
  
   render() {
     return (
-      <footer className="footer is-success">
+      <footer className="footer">
         <div className=" footerContent">
-          <img src="https://image.flaticon.com/icons/svg/174/174848.svg"/>
-          <img src="https://image.flaticon.com/icons/svg/174/174855.svg"/>
-          <img src="https://image.flaticon.com/icons/svg/733/733579.svg"/>
-          <img src="https://image.flaticon.com/icons/svg/174/174883.svg"/>
+          <img src="https://image.flaticon.com/icons/svg/174/174848.svg" id="icon"/>
+          <img src="https://image.flaticon.com/icons/svg/174/174855.svg" id="icon"/>
+          <img src="https://image.flaticon.com/icons/svg/733/733579.svg" id="icon"/>
+          <img src="https://image.flaticon.com/icons/svg/174/174883.svg" id="icon"/>
         </div>
       </footer>
     )
