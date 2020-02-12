e64cadeeb906ebda580e4822341da2b322abb8d4
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-12 17:04:09 +0000

    savedhart

--- react-project-starterpack/src/components/trails/TrailShow.js
+++ react-project-starterpack/src/components/trails/TrailShow.js
@@ -114,7 +114,7 @@ class TrailShow extends React.Component {
           <hr />
           <div className="columns">
             <div className="column is-half">
-              <button onClick={this.handleClick} className="button is-danger">
+              <button onClick={this.handleSave} className="button is-danger">
                 <span className="icon is-small">
                   <FontAwesomeIcon icon={faHeart} />
                 </span>
@@ -128,7 +128,7 @@ class TrailShow extends React.Component {
               </button>
               <div className="Mapbox">
                 <br />
-
+              
                 <IdMap
                   data={{
                     latitude: trail.latitude,
