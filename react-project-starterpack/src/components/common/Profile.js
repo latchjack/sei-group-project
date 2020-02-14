aa97bf65a8ad75ef3d895ace4eb2f64b2fdb3c95
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-14 12:41:47 +0000

    icons

--- react-project-starterpack/src/components/common/Profile.js
+++ react-project-starterpack/src/components/common/Profile.js
@@ -24,46 +24,58 @@ class Profile extends React.Component {
   render() {
     const { profile } = this.state
     return (
-      <div className="column is-mobile is-multiline">
-        <section className="section" id="profileSection">
-          <h2 className=" title is-3">{profile.username}&apos;s profile</h2>
-          <div className="container">
+      <div className="box" id="profileBox">
+        <div className="column is-mobile is-multiline">
+          <section className="section" id="profileSection">
+            <div>
+              <i className="fas fa-map-marked-alt"></i>
+              {profile.username}&apos;s profile
+            </div>
             
-            <h2 className="is-size-4">Trails {profile.username} has made</h2>
-            <div>&nbsp;</div> 
-            <div className="columns is-mobile is-multiline">
-              {profile.createdTrails && profile.createdTrails.length === 0 && 
+                  
+            <div className="container">
+              <div className="box">
+                <h2 className="is-size-4">Trails {profile.username} has made</h2>
+                <div>&nbsp;</div> 
+                <div className="columns is-mobile is-multiline">
+                  {profile.createdTrails && profile.createdTrails.length === 0 && 
             <p className="subtitle is-5">You haven&apos;t created any trails yet!</p>}
-              {profile.createdTrails && profile.createdTrails.map(trail => (
-                <TrailCard key={trail._id} {...trail} />
-              ))}
+                  {profile.createdTrails && profile.createdTrails.map(trail => (
+                    <TrailCard key={trail._id} {...trail} />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div className="container">
-            <h2 className="is-size-4">Saved Trails</h2>
-            <div>&nbsp;</div> 
-            <div className="columns is-mobile is-multiline">
-              {profile.likedTrails && profile.likedTrails.length === 0 && 
+            <hr />
+            <div className="container">
+              <div className="box">
+                <h2 className="is-size-4">Saved Trails</h2>
+                <div>&nbsp;</div> 
+                <div className="columns is-mobile is-multiline">
+                  {profile.likedTrails && profile.likedTrails.length === 0 && 
             <p className="subtitle is-5">&nbsp;&nbsp;You have not saved any trails yet!</p>}
-              {profile.likedTrails && profile.likedTrails.map(trail => (
-                <TrailCard key={trail._id} {...trail} />
-              ))}
+                  {profile.likedTrails && profile.likedTrails.map(trail => (
+                    <TrailCard key={trail._id} {...trail} />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div className="container">
-            <h2 className="is-size-4">Completed Trails</h2>
-            <div>&nbsp;</div> 
-            <div className="columns is-mobile is-multiline">
-              {profile.completedTrails && profile.completedTrails.length === 0 && 
-            <p className="subtitle is-5">You have not completed any trails yet!</p>}
-              {profile.completedTrails && profile.completedTrails.map(trail => (
-                <TrailCard key={trail._id} {...trail} />
-              ))}
+            <hr />
+            <div className="container">
+              <div className="box">
+                <h2 className="is-size-4">Completed Trails</h2>
+                <div>&nbsp;</div> 
+                <div className="columns is-mobile is-multiline">
+                  {profile.completedTrails && profile.completedTrails.length === 0 && 
+            <p className="subtitle is-5">&nbsp;&nbsp;You have not completed any trails yet!</p>}
+                  {profile.completedTrails && profile.completedTrails.map(trail => (
+                    <TrailCard key={trail._id} {...trail} />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     )
   }
