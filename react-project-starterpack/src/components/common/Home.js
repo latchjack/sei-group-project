30b4c7819ce7e42b4aee26e866039b76b0c702cc
Latch Jack <latchlernj@gmail.com>
2020-02-14 12:37:42 +0000

    push

--- react-project-starterpack/src/components/common/Home.js
+++ react-project-starterpack/src/components/common/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Geotext from './../../assets/geo_textonly.svg'
 
 
 const Home = () => (
@@ -6,7 +7,7 @@ const Home = () => (
   <section className="hero is-fullheight-with-navbar is-primary">
     <div className="hero-body">
       <div className="container">
-        <p className="title is-1 has-text-centered" id="Hero_txt">GEO-CACHR</p>
+        <p className="title is-1 has-text-centered" id="Hero_txt"><span className="geo-text"><img src={Geotext} /></span></p>
         <p className=" title is-2 has-text-centered " id="Hero_sub">The largest treasure Hunt Community in London!</p>
       </div>
     </div>
