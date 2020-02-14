9fb1a42964ffe21c72ba28c6be2176b24ff97bb2
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-14 12:11:44 +0000

    layout stuff

--- react-project-starterpack/src/components/common/Contact.js
+++ react-project-starterpack/src/components/common/Contact.js
@@ -11,11 +11,24 @@ class Contact extends React.Component {
      
         <div className="box" id="contactBox">
           <br/>
-        
+          <h1 className=" title is-3">CONTACT US</h1>
           <div className="columns is-multiline is-mobile">
+            <div className="column is half">
+              <h3 className="is-size-4">The Team</h3>
+              <div>&nbsp;</div> 
+              <img src={Teamgeocache} id="theGang" />      
+              <div>&nbsp;</div> 
+              <h3 className="is-size-4">Email</h3>
+              <a href="mailto:contact@geocachr.com"><p className="FAQtxt"> contact@geocachr.com</p></a>
+              <div>&nbsp;</div> 
+              <h3 className="is-size-4">Phone Number</h3>
+              <p className="FAQtxt">01443 567982</p>
+              <hr/>
+              <hr/>
+            </div>
             <div className="column is-half">
-              <h1 className=" title is-3">CONTACT US</h1>
-              <h3 className="is-size-3">Address</h3>
+             
+              <h3 className="is-size-4">Address</h3>
               <ul className="FAQtxt">
                 <li>45 Made Up Street</li>
                 <li>London</li>
@@ -30,21 +43,6 @@ class Contact extends React.Component {
                 />
               </div>
             
-            </div>
-            <div className="column is half">
-            
-              <h3 className="is-size-3">The Team</h3>
-              <div>&nbsp;</div> 
-              <img src={Teamgeocache} id="theGang" />
-                    
-              <hr/>
-              <h3 className="is-size-3">Email</h3>
-              <a href="mailto:contact@geocachr.com"><p className="FAQtxt"> contact@geocachr.com</p></a>
-              <hr/>
-          
-             
-            
-              <hr/>
             </div>
           </div>
         </div> 
--- react-project-starterpack/src/components/common/Profile.js
+++ react-project-starterpack/src/components/common/Profile.js
@@ -25,41 +25,45 @@ class Profile extends React.Component {
     const { profile } = this.state
     return (
       <div className="column is-mobile is-multiline">
-      <section className="section">
-        <h1 className="title">{profile.username}&apos;s profile</h1>
-        <div className="container">
-          <h2 className="title is-4">Trails that {profile.username} has made:</h2>
-          <div className="columns is-mobile is-multiline">
-            {profile.createdTrails && profile.createdTrails.length === 0 && 
-            <p className="subtitle">You haven&apos;t created any trails yet!</p>}
-            {profile.createdTrails && profile.createdTrails.map(trail => (
-              <TrailCard key={trail._id} {...trail} />
-            ))}
+        <section className="section" id="profileSection">
+          <h2 className=" title is-3">{profile.username}&apos;s profile</h2>
+          <div className="container">
+            
+            <h2 className="is-size-4">Trails {profile.username} has made</h2>
+            <div>&nbsp;</div> 
+            <div className="columns is-mobile is-multiline">
+              {profile.createdTrails && profile.createdTrails.length === 0 && 
+            <p className="subtitle is-5">You haven&apos;t created any trails yet!</p>}
+              {profile.createdTrails && profile.createdTrails.map(trail => (
+                <TrailCard key={trail._id} {...trail} />
+              ))}
+            </div>
           </div>
-        </div>
-        <hr />
-        <div className="container">
-          <h2 className="title is-4">Saved Trails:</h2>
-          <div className="columns is-mobile is-multiline">
-            {profile.likedTrails && profile.likedTrails.length === 0 && 
-            <p className="subtitle">You have not saved any trails yet!</p>}
-            {profile.likedTrails && profile.likedTrails.map(trail => (
-              <TrailCard key={trail._id} {...trail} />
-            ))}
+          <hr />
+          <div className="container">
+            <h2 className="is-size-4">Saved Trails</h2>
+            <div>&nbsp;</div> 
+            <div className="columns is-mobile is-multiline">
+              {profile.likedTrails && profile.likedTrails.length === 0 && 
+            <p className="subtitle is-5">&nbsp;&nbsp;You have not saved any trails yet!</p>}
+              {profile.likedTrails && profile.likedTrails.map(trail => (
+                <TrailCard key={trail._id} {...trail} />
+              ))}
+            </div>
           </div>
-        </div>
-        <hr />
-        <div className="container">
-          <h2 className="title is-4">Completed Trails:</h2>
-          <div className="columns is-mobile is-multiline">
-            {profile.completedTrails && profile.completedTrails.length === 0 && 
-            <p className="subtitle">You have not completed any trails yet!</p>}
-            {profile.completedTrails && profile.completedTrails.map(trail => (
-              <TrailCard key={trail._id} {...trail} />
-            ))}
+          <hr />
+          <div className="container">
+            <h2 className="is-size-4">Completed Trails</h2>
+            <div>&nbsp;</div> 
+            <div className="columns is-mobile is-multiline">
+              {profile.completedTrails && profile.completedTrails.length === 0 && 
+            <p className="subtitle is-5">You have not completed any trails yet!</p>}
+              {profile.completedTrails && profile.completedTrails.map(trail => (
+                <TrailCard key={trail._id} {...trail} />
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
       </div>
     )
   }
