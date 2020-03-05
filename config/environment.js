2c3b8e20ac21f2886a827d2f2961864d02b25f01
Latch Jack <latchlernj@gmail.com>
2020-03-05 13:28:45 +0000

    changed environment.js

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGOD_DB || `mongodb://localhost/trail-cache-api-${env}`
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/trail-cache-api-${env}`
 const secret = process.env.SECRET || 'oh my gosh I want to know'
 
 module.exports = { port, dbURI, secret }
